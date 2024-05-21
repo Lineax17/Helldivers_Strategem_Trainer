7c13b75d50dbb976f7bdd3000c5399ce4260c9f7
Lineax17 <philipp.hartmann04@proton.me>
2024-05-21 17:04:58 +0200

    Created Helldivers_Strategem_Trainer

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+
+//document.getElementById("start-button").addEventListener("click", function() {})
+
+function startTraining() {
+    alert("Prepare yourself. Your training is starting now!")
+    window.location.href = "trainer.html"
+}
+
