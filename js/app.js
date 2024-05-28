9bc5aef1e8d3e4479cdfd38fc8be09b6dde8bb36
Lineax17 <philipp.hartmann04@proton.me>
2024-05-28 19:24:57 +0200

    Updating Project

--- js/app.js
+++ js/app.js
@@ -1,15 +1,63 @@
 console.log('Hello World')
-main("500KG")
 
-function main(string) {
-    const fs = require('fs')
+strategems = [
+    {
+        "name": "Eagle_500KG",
+        "keys": [
+          "w", "d", "s", "s", "s"
+        ]
+    },
+    {
+        "name": "Eagle_Airstrike",
+        "code": ["w", "d", "s", "d"]
+    }
+]
 
-    let rawdata = fs.readFileSync('strategems.json', 'utf8')
-    let strategem = JSON.parse(rawdata)
-    let keys = strategem.keys
-    console.log(keys)
+
+function main(number) {
+    let data = strategems[number] 
+    let buttonsToPress = data.keys;
+    console.log(buttonsToPress)
+
+    var newWindow = window.open('', '_blank');
+
+    //append css
+    var link = newWindow.document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '../css/style.css'; // Pfad zur CSS-Datei
+    newWindow.document.head.appendChild(link);
+
+    //append body
+    for (var i = 0; i < buttonsToPress.length; i++) {
+        var img = document.createElement('img');
+
+        if (buttonsToPress[i] == "w") {
+            img.src = 'media/arrows/Up_Unpressed.webp'
+        } else if (buttonsToPress[i] == "a"){
+            img.src = 'media/arrows/Left_Unpressed.webp'
+        } else if (buttonsToPress[i] == "s"){
+            img.src = 'media/arrows/Down_Unpressed.webp'
+        } else if (buttonsToPress[i] == "d"){
+            img.src = 'media/arrows/Right_Unpressed.webp'
+        }
+        newWindow.document.body.appendChild(img);
+    }
+
+
+    for (var i = 0; i < buttonsToPress.length; i++) {
+        newWindow.addEventListener('keydown', function(event) {
+        
+            let key = event.key.toLowerCase();
+            console.log("Der Benutzer hat die Taste gedrückt: " + key);
+    
+            if (buttonsToPress[numberOfArrow] == key){
+                return true
+            } else {
+                return false
+            }
+        });
+    }
+    
 }
-
-
 
 
