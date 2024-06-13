eac7d49b0898c546647f074892a025adfe27230c
Lineax17 <philipp.hartmann04@proton.me>
2024-06-13 19:35:44 +0200

    Updating Project

--- js/app.js
+++ js/app.js
@@ -2,42 +2,49 @@ console.log('Hello World')
 
 strategems = [
     //Bridge
+    //0
     {
         "name": "HMG_Emplacement",
         "keys": [
             "s","w","a","d","d","a"
         ]
     },
+    //1
     {
         "name": "Orbital_EMS_Strike",
         "keys": [
             "d","d","a","s"
         ]
     },
+    //2
     {
         "name": "Orbital_Gas_Strike",
         "keys": [
             "d","d","s","d"
         ]
     },
+    //3
     {
         "name": "Orbital_Precision_Strike",
         "keys": [
             "d","d","w"
         ]
     },
+    //4
     {
         "name": "Orbital_Smoke_Strike",
         "keys": [
             "d","d","s","w"
         ]
     },
+    //5
     {
         "name": "Shield_Generator_Relay",
         "keys": [
             "s","s","a","d","a","d"
         ]
     },
+    //6
     {
         "name": "Tesla_Tower",
         "keys": [
@@ -47,6 +54,7 @@ strategems = [
 
 
     //Engineering Bay
+    //7
     {
         "name": "Anti-Personnel_Minefield",
         "keys": [
@@ -61,55 +69,63 @@ strategems = [
         ]
     },
     */
-
+    //8
     {
         "name": "Arc_Thrower",
         "keys": [
             "s","d","s","w","a","a"
         ]
     },
+    //9
     {
         "name": "Ballistic_Shield_Backpack",
         "keys": [
             "s","a","s","s","w","a"
         ]
     },
+    //10
     {
         "name": "Grenade_Launcher",
         "keys": [
             "s","a","w","a","s"
         ]
     },
+    //11
     {
         "name": "Guard_Dog_Rover",
         "keys": [
             "s","w","a","w","d","d"
         ]
     },
+    //12
     {
         "name": "Incindiary_Mines",
         "keys": [
             "s","a","a","s"
         ]
     },
+    //13
     {
         "name": "Laser_Cannon",
         "keys": [
             "s","a","s","w","a"
         ]
     },
+    //14
     {
         "name": "Quasar_Cannon",
         "keys": [
             "s","s","w","a","d"
         ]
     },
+    //15
     {
         "name": "Shield_Generator_Pack",
         "keys": [
             "s","w","a","d","a","d"
         ]
     },
+    //16
     {
         "name": "Supply_Pack",
         "keys": [
@@ -120,54 +136,63 @@ strategems = [
 
 
     //Hangar
+    //17
     {
         "name": "Eagle_Airstrike",
         "keys": [
             "w","d","s","d"
         ]
     },
+    //18
     {
         "name": "Eagle_Cluster_Bomb",
         "keys": [
             "w","d","s","s","d"
         ]
     },
+    //19
     {
         "name": "Eagle_Napalm_Strike",
         "keys": [
             "w","d","s","w"
         ]
     },
+    //20
     {
         "name": "Eagle_Rearm",
         "keys": [
             "w","w","a","w","d"
         ]
     },
+    //21
     {
         "name": "Eagle_Smoke_Strike",
         "keys": [
             "w","d","w","s"
         ]
     },
+    //22
     {
         "name": "Eagle_Strafing_Run",
         "keys": [
             "w","d","d"
         ]
     },
+    //23
     {
         "name": "Eagle_110MM_Rocket_Pods",
         "keys": [
             "w","d","w","a"
         ]
     },
+    //24
     {
         "name": "Eagle_500KG_Bomb",
         "keys": [
             "w","d","s","s","s"
         ]
     },
+    //25
     {
         "name": "Jump_Pack",
         "keys": [
@@ -179,42 +204,49 @@ strategems = [
 
 
     //Orbital_Cannons
+    //26
     {
         "name": "Orbital_Airburst_Strike",
         "keys": [
             "d","d","d"
         ]
     },
+    //27
     {
         "name": "Orbital_Gatling_Barrage",
         "keys": [
             "d","s","a","w","w"
         ]
     },
+    //28
     {
         "name": "Orbital_Laser",
         "keys": [
             "d","s","w","d","s"
         ]
     },
+    //29
     {
         "name": "Orbital_Railcannon_Strike",
         "keys": [
             "d","w","s","s","d"
         ]
     },
+    //30
     {
         "name": "Orbital_Walking_Barrage",
         "keys": [
             "d","s","d","s","d","s"
         ]
     },
+    //31
     {
         "name": "Orbital_120MM_HE_Barrage",
         "keys": [
             "d","d","s","a","d","s"
         ]
     },
+    //32
     {
         "name": "Orbital_380MM_HE_Barrage",
         "keys": [
@@ -224,66 +256,77 @@ strategems = [
 
 
     //Patriotic Administration Center
+    //33
     {
         "name": "Airburst_Rocket_Launcher",
         "keys": [
             "s","w","w","a","d"
         ]
     },
+    //34
     {
         "name": "Anti-Materiel_Rifle",
         "keys": [
             "s","a","d","w","s"
         ]
     },
+    //35
     {
         "name": "Autocannon",
         "keys": [
             "s","a","s","w","w","d"
         ]
     },
+    //36
     {
         "name": "Expendable_Anti-Tank",
         "keys": [
             "s","s","a","w","d"
         ]
     },
+    //37
     {
         "name": "Flamethrower",
         "keys": [
             "s","a","w","s","w"
         ]
     },
+    //38
     {
         "name": "Heavy_Machine_Gun",
         "keys": [
             "s","a","w","s","s"
         ]
     },
+    //39
     {
         "name": "Machine_Gun",
         "keys": [
             "s","a","s","w","d"
         ]
     },
+    //40
     {
         "name": "Railgun",
         "keys": [
             "s","d","s","w","a","d"
         ]
     },
+    //41
     {
         "name": "Recoilless_Rifle",
         "keys": [
             "s","a","d","d","a"
         ]
     },
+    //42
     {
         "name": "Spear",
         "keys": [
             "s","s","w","s","s"
         ]
     },
+    //43
     {
         "name": "Stalwart",
         "keys": [
@@ -294,54 +337,63 @@ strategems = [
 
 
     //Robotics Workshop
+    //44
     {
         "name": "Autocannon_Sentry",
         "keys": [
             "s","w","d","w","a","w"
         ]
     },
+    //45
     {
         "name": "Emancipator_Exosuit",
         "keys": [
             "a","s","d","w","a","s","w"
         ]
     },
+    //46
     {
         "name": "EMS_Mortar_Sentry",
         "keys": [
             "s","w","d","s","d"
         ]
     },
+    //47
     {
         "name": "Gatling_Sentry",
         "keys": [
             "s","w","d","a"
         ]
     },
+    //48
     {
         "name": "Guard_Dog",
         "keys": [
             "s","w","a","w","d","s"
         ]
     },
+    //49
     {
         "name": "Machine_Gun_Sentry",
         "keys": [
             "s","w","d","d","w"
         ]
     },
+    //50
     {
         "name": "Mortar_Sentry",
         "keys": [
             "s","w","d","d","s"
         ]
     },
+    //51
     {
         "name": "Patriot_Exosuit",
         "keys": [
             "a","s","d","w","a","s","s"
         ]
     },
+    //52
     {
         "name": "Rocket_Sentry",
         "keys": [
@@ -351,72 +403,84 @@ strategems = [
 
 
     //Mission Stratagems
+    //53
     {
         "name": "Dark_Fluid_Vessel",
         "keys": [
             "w","a","d","s","w","w"
         ]
     },
+    //54
     {
         "name": "Hellbomb",
         "keys": [
             "s","w","a","s","w","d","s","w"
         ]
     },
+    //55
     {
         "name": "Orbital_Illumination_Flare",
         "keys": [
             "d","d","a","a"
         ]
     },
+    //56
     {
         "name": "Prospecting_Drill",
         "keys": [
             "s","s","a","d","s","s"
         ]
     },
+    //57
     {
         "name": "Reinforce",
         "keys": [
             "w","s","d","a","w"
         ]
     },
+    //58
     {
         "name": "Resupply",
         "keys": [
             "s","s","w","d"
         ]
     },
+    //59
     {
         "name": "SEAF_Artillery",
         "keys": [
             "d","w","w","s"
         ]
     },
+    //60
     {
         "name": "Seismic_Probe",
         "keys": [
             "w","w","a","d","s","s"
         ]
     },
+    //61
     {
         "name": "SOS_Beacon",
         "keys": [
             "w","s","d","w"
         ]
     },
+    //62
     {
         "name": "Super_Earth_Flag",
         "keys": [
             "s","w","s","w"
         ]
     },
+    //63
     {
         "name": "Tectonic_Drill",
         "keys": [
             "w","s","w","s","w","s"
         ]
     },
+    //64
     {
         "name": "Upload_Data",
         "keys": [
@@ -447,6 +511,39 @@ function startTraining(number) {
             this.location.reload()
         }
     })
+}
+
+function generateTraining(number) {
+    var newWindow = window.open('', '_blank', 'width=600,height=400');
+
+    newWindow.document.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <link rel="stylesheet" href="css/style.css">
+            <script src="js/app.js"></script>
+        </head>
+        <body>
+    
+        </body>
+        </html>`
+    )
+
+    newWindow.document.close()
+
+    let data = strategems[number]
+    let buttonsToPress = data.keys
+    let name = data.name
+    let index = 0
+
+    var img = newWindow.document.createElement('h2')
+    h2.textcontent = name
+    header.appendChild(h2)
+
+    
 }
 
 
